9a76d51551a3ea2f125bf4eaafc5977b416f4137
Rhushil Vasavada <126303607+generalAlkazer@users.noreply.github.com>
2025-02-11 21:52:28 -0600

    comments

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -1,11 +1,19 @@
+#Author: Rhushil Vasavada
+#Hand-tracking MediaPipe Module
+#Description: This program models a custom hand detection class to detect 21 hand landmarks as a building block for
+#future programs that use this to solve more complex tasks, such as finger counting.
+
+#import libraries
 import mediapipe as mp
 import cv2
 import time
 import random
 
-
+#define class to be used to create a hand detector
 class handDetector():
     def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+        #define variables to keep track of custom parameters such as max hands allowed to be detected and detection 
+        #confidence (how confident the model is labeling a specific hand landmark)
         self.mode = mode
         self.maxHands = maxHands
         self.modelComplexity = modelComplexity
@@ -18,6 +26,7 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
+        #use MediaPipe's built-in library library to detect the landmarks in a given frame image
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
         if self.results.multi_hand_landmarks:
@@ -29,7 +38,7 @@ class handDetector():
     def findPosition(self, img, handNo=0, draw=True):
 
         lmList = []
-
+        #find the positions of landmarks on a given detected hand
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
@@ -46,6 +55,7 @@ class handDetector():
 
 
 def main():
+    #configure webcam and run this hand detection class on live camera footage
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
@@ -63,6 +73,7 @@ def main():
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
+        #display output with landmarks drawn over screen
         img = cv2.flip(img,1)
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Image", img)
